ab0f58a46d4440e95cc217156e07cb8fc4e1d4de
openXu <openXu@yeah.net>
2019-02-27 19:08:58 +0800

    ViewModel封装

--- library_base/src/main/java/com/fzy/libs/base/BaseActivity.java
+++ library_base/src/main/java/com/fzy/libs/base/BaseActivity.java
@@ -11,7 +11,14 @@ import androidx.databinding.DataBindingUtil;
 import androidx.databinding.ViewDataBinding;
 import androidx.lifecycle.ViewModelProviders;
 
-
+/**
+ * Author: openXu
+ * Time: 2019/2/27 15:51
+ * class: BaseActivity
+ * Description:
+ *
+ *
+ */
 public abstract class BaseActivity<V extends ViewDataBinding, VM extends BaseViewModel> extends AppCompatActivity {
 
     protected String TAG;
@@ -47,7 +54,7 @@ public abstract class BaseActivity<V extends ViewDataBinding, VM extends BaseVie
     //页面数据初始化方法
     protected abstract void initData();
 
-    /**注入DataBinding*/
+    /** 注入DataBinding，初始化ViewModel */
     private void initViewDataBinding(Bundle savedInstanceState) {
         //DataBindingUtil类需要在project的build中配置 dataBinding {enabled true }, 同步后会自动关联android.databinding包
         binding = DataBindingUtil.setContentView(this, getContentView(savedInstanceState));
